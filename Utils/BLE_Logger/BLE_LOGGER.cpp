720d7b89e502c8a7105ac3466d3ba494d8a5ae5a
Bored_Leaf_Linux <zachary1.voce@gmail.com>
2026-03-02 17:38:26 +1100

    coloured terminal text

--- Utils/BLE_Logger/BLE_LOGGER.cpp
+++ Utils/BLE_Logger/BLE_LOGGER.cpp
@@ -3,5 +3,6 @@
 #include "BLE_LOGGER.h"
 
 void print_message(const std::string &message) {
-    std::print("{}\n", message);
+    // TODO:: Wrap escape sequencies to make more readable
+    std::print("\033[31m[ERROR]\033[0m {}\n", message);
 }
