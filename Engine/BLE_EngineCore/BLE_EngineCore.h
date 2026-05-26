6e3e5224061f3886a7844b46761151ce6f906822
Bored_Leaf_Linux <zachary1.voce@gmail.com>
2026-05-27 00:59:12 +1000

    Added BLE_API macro

--- Engine/BLE_EngineCore/BLE_EngineCore.h
+++ Engine/BLE_EngineCore/BLE_EngineCore.h
@@ -1,12 +1,23 @@
 #pragma once
+
+#if defined(_WIN32)
+    #if defined(BLE_BUILD_DLL)
+        #define BLE_API __declspec(dllexport)
+    #else
+        #define BLE_API __declspec(dllimport)
+    #endif
+#else
+    #define BLE_API __attribute__((visibility("default")))
+#endif
+
 #include <memory>
 
 class EngineCore {
 public:
-    EngineCore();
-    ~EngineCore();
-    void initialise();
-    void shutdown();
+    BLE_API EngineCore();
+    BLE_API ~EngineCore();
+    BLE_API void initialise();
+    BLE_API void shutdown();
 
     // TODO: Register functions only when using dlopen/dlsym
 private:
